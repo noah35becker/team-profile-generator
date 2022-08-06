23fd9c70bcc031607f11fecabe00fc1671d59df7
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-08-06 02:15:53 -0400

    menu functionality

--- src/userPrompt.js
+++ src/userPrompt.js
@@ -2,8 +2,22 @@
 const inquirer = require('inquirer');
 const [Engineer, Intern, Manager] = [require('../lib/Engineer'), require('../lib/Intern'), require('../lib/Manager')];
 
-
 const TAB = '    '; // 4 spaces
+const ANSI = {
+    bold: {
+        set: '\033[1m',
+        reset: '\033[0m'
+    },
+    color: {
+        yellow: '\u001b[33m',
+        magenta: '\u001b[35m',
+        reset: '\u001b[0m'
+    }
+};
+
+var employees = [];
+
+
 
 
 // Inquirer prompt, prepending results so far
@@ -14,54 +28,71 @@ const inquirerPrompt = (resultsSoFar, questions) => new Promise(resolve => {
 
 
 const makeNewEmployee = (role) => new Promise(resolve => {
-    console.log(role + ':');
+    console.log(`${ANSI.bold.set}${ANSI.color.magenta}${role}:${ANSI.bold.reset}${ANSI.color.reset}`);
     
     inquirerPrompt({}, [
         {
             type: 'input',
             name: 'name',
-            message: `${TAB}What's your name?`
+            message: `${TAB}Enter name:`
         },
         {
             type: 'input',
             name: 'id',
-            message: `${TAB}What's your employee ID?`
+            message: `${TAB}Enter employee ID:`
         },
         {
             type: 'input',
             name: 'email',
-            message: `${TAB}What's your email address?`
+            message: `${TAB}Enter email address:`
         },
     ]).then(resultsSoFar => {
         if (role === 'Manager')
             return inquirerPrompt(resultsSoFar, {
                 type: 'input',
                 name: 'officeNumber',
-                message: `${TAB}What's your office number?`
+                message: `${TAB}Enter office number:`
             });
         else if (role === 'Engineer')
             return inquirerPrompt(resultsSoFar, {
                 type: 'input',
                 name: 'github',
-                message: `${TAB}What's your GitHub account?`
+                message: `${TAB}Enter GitHub account:`
             });
         else if (role === 'Intern')
             return inquirerPrompt(resultsSoFar, {
                 type: 'input',
                 name: 'school',
-                message: `${TAB}What's school did you attend?`
+                message: `${TAB}Enter school:`
             });
     }).then(({id, name, email, ...other}) => {
         if (role === 'Manager')
-            resolve(new Manager(id, name, email, other.officeNumber));
+            resolve(employees.push(new Manager(id, name, email, other.officeNumber)));
         else if (role === 'Engineer')
-            resolve(new Engineer(id, name, email, other.github));
+            resolve(employees.push(new Engineer(id, name, email, other.github)));
         else if (role === 'Intern')
-            resolve(new Intern(id, name, email, other.school));
+            resolve(employees.push(new Intern(id, name, email, other.school)));
     });
     ;
 });
 
 
-makeNewEmployee('Intern')
-.then(newObj => console.log(newObj));
+const menu = () => new Promise(resolve => {
+    inquirerPrompt({}, {
+        type: 'list',
+        name: 'next',
+        message: `${ANSI.color.yellow}What would you like to do next?${ANSI.color.reset}`,
+        choices: ['Enter an engineer', 'Enter an intern', "I've finished building my team"]
+    })
+    .then(({next}) => {
+        if (next === 'Enter an engineer')
+            resolve(makeNewEmployee('Engineer'));
+        else if (next === 'Enter an intern')
+            resolve(makeNewEmployee('Intern'));
+        else
+            ; // ADD CODE FOR "FINISHED BUILDING MY TEAM"
+    });
+});
+
+
+menu();
